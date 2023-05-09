ef348bbac3769d48026f636d795f02e00e4b8a86
jic <ji_camargo@yahoo.com>
2023-05-09 11:46:19 -0500

    add local dev server

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ function component() {
   const element = document.createElement('div');
 
   // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello ', 'Webpack and Microverse'], ' ');
+  element.innerHTML = _.join(['Hello', 'Webpack and Microverse', 'This is a powerful excersice'], ' ');
   element.classList.add('hello');
   
   return element;
